680a291f773f8db814f852bdde5a126bf9416fa8
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-26 15:37:57 +0100

    Update Token.php

--- src/Core/Abstraction/Token.php
+++ src/Core/Abstraction/Token.php
@@ -5,12 +5,35 @@
 	abstract class Token
 	{
 		private $startIndex = 0;
-		private $endIndex = 0;		
+		private $endIndex = 0;	
+		
+		private $parentScope = null;
+		private $childScopes = [];
 		
 		private $originalJavascript = '';
 		
-		//setters and getters
+		public function parse(string $jsCode) : Token{
+			
+			#TODO Implement Parser
+			
+			return $this;
+		}
 		
+		public function addChildScope(Token $scope) : Token{
+		   	$this->childScopes[] = $scope;
+			return $this;
+		}
+		public function getChildScopes() : array{
+			return $this->childScopes;	
+		}
+		//setters and getters
+		public function setParent(Token $token) : Token{
+			$this->parentScope = $token;
+			return $this;
+		}
+		public function getParent() : Token{
+			return $this->parentScope;	
+		}
 		public function setCode(string $code) : Token{
 			$this->originalJavascript = $code;
 			return $this;
