f62f8e81a43bb23ae9afafb298de57822d138d04
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-26 20:24:15 +0100

    Update Prototype.php

--- src/Javascript/Prototype.php
+++ src/Javascript/Prototype.php
@@ -5,8 +5,12 @@
     class Prototype extends NativeObject
     {
         static $__proto__ = null; // stays null, a prototype cannot have a prototype
-        
-        
+       
+        public function __construct()
+        {
+            // this prevents the prototype being initialised
+        }
+
     }
 
 ?>
