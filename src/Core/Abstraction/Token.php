079bb46f926da1dae9e1ac41cf726dcd08b3f13a
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-26 15:23:44 +0100

    Update Token.php

--- src/Core/Abstraction/Token.php
+++ src/Core/Abstraction/Token.php
@@ -4,15 +4,34 @@
 
 	abstract class Token
 	{
-		/**
-		* @return {Integer} 
-		* @description - while iterating the JavaScript string, this will return 
-		* the position in which the current token ends, so the iterator can skip 
-		* all the characters in this token.
-		*/
-		public abstract function getEndOfExpression() : int;
+		private $startIndex = 0;
+		private $endIndex = 0;		
 		
+		private $originalJavascript = '';
 		
+		//setters and getters
+		
+		public function setCode(string $code) : Token{
+			$this->originalJavascript = $code;
+			return $this;
+		}
+		public function getCode() : string{
+			return $this->originalJavascript;	
+		}
+		public function setStartIndex( int $index ) : Token{
+			$this->startIndex = $index;
+			return $this;
+		}
+		public function getStartIndex() : int{
+			return $this->startIndex;	
+		}
+		public function setEndIndex( int $index ) : Token{
+			$this->endIndex = $index;
+			return $this;
+		}
+		public function getEndIndex() : int{
+			return $this->endIndex;
+		}
 	}
 
 ?>
