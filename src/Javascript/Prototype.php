4ce4623d4e92fd7ba43e479378953c6917bc06af
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-26 20:21:39 +0100

    Create Prototype.php

---
+++ src/Javascript/Prototype.php
@@ -0,0 +1,12 @@
+<?php
+  
+    namespace PHPJS\Javascript;
+
+    class Prototype extends NativeObject
+    {
+        static $__proto__ = null; // stays null, a prototype cannot have a prototype
+        
+        
+    }
+
+?>
