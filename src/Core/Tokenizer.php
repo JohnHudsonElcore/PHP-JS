c2ca8910138880303228cfa71a49575a8aaf35a8
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-26 14:45:30 +0100

    Create Tokenizer.php

---
+++ src/Core/Tokenizer.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace PHPJS\Core;
+
+    class Tokenizer
+    {
+        public function __construct()
+        {
+            #TODO: Implement __construct   
+        }
+        public function tokenize($jsCode)
+        {
+               
+        }            
+    }
+
+?>
