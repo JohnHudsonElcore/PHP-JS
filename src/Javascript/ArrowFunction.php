3718ab4cdd6baaa1a339335444b8eaa4c4042665
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-27 10:34:00 +0100

    Create ArrowFunction.php

---
+++ src/Javascript/ArrowFunction.php
@@ -0,0 +1,12 @@
+<?php
+    
+    namespace PHPJS\Javascript;
+
+    class ArrowFunction extends BaseFunction
+    {
+          //seperate these functions from real functions
+          //this is a debug tool, when var_dump is used.
+          private $isLambda = true;
+    }
+
+?>
